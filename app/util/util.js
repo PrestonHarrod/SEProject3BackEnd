f553e65e2d3b92be06a566ca3323aaa0a00a4ec1
Jared Torp <jared.torp@eagles.oc.edu>
2021-10-28 17:10:36 -0500

    feat: auth util file. having some curlybrace errors

--- app/util/util.js
+++ app/util/util.js
@@ -111,7 +111,7 @@ isAdmin = (req, res, next) => {
   }
 }
 
-isAdvisor = (req, res, next) => {
+isAdvisorOrAdmin = (req, res, next) => {
   let authheader = req.get("authorization");
   if (authheader!=null) //we need to make sure that the authheader is not null
   {
@@ -136,47 +136,121 @@ isAdvisor = (req, res, next) => {
         {
           //find the advisorID in the session
           if(session.advisorID != null){
-            Admin.findByPk(session.advisorID)
+            Advisor.findByPk(session.advisorID)
             .then(data => {
               if(data.dataValues.role = "Advisor")
               {
                 next();
                 return;
               }
-              else {
-                //error
+              //no else statement here because it is ok if null
+
+
+            })
+          }
+          else if(session.advisorID != null){
+            Admin.findByPk(session.adminID)
+            .then(data => {
+              if(data.dataValues.role = "Admin")
+              {
+                next();
+                return;
               }
 
             })
           }
+        else{
+            //error does not have role
+
         }
-       })
+       
+      
+    }
+    else{
+      //Error
+    }
+  }
+
+}
+
+
+
+isAny = (req, res, next) => {
+  let authheader = req.get("authorization");
+  if (authheader!=null) //we need to make sure that the authheader is not null
+  {
+    if (authheader.startsWith("Bearer ")){ //dont accept a header that 
+      let token = authHeader.substring(7, authHeader.length);
+      jwt.verify(token, config.secret), (err, decoded) => { //verify the token
+        if (err){
+          return res.json({
+            success:false,
+            message: 'token is not valid'
+          })
+        }
+      }
+      //find the session with the same token, sessions are created in the auth controller when one is logged in
+      Session.findOne({
+        where : {token:token}
+      })
+      .then(data=> {
+        //check to see if session has the admin
+        let session = data.dataValues;
+        if (session!= null)
+        {
+          //find the advisorID in the session
+          if(session.advisorID != null){
+            Advisor.findByPk(session.advisorID)
+            .then(data => {
+              if(data.dataValues.role = "Advisor")
+              {
+                next();
+                return;
+              }
+              //no else statement here because it is ok if null
+
+
+            })
+          }
+          else if(session.adminID != null){
+            Admin.findByPk(session.adminID)
+            .then(data => {
+              if(data.dataValues.role = "Admin")
+              {
+                next();
+                return;
+              }
+
+            })
+        }
+        else if (session.studentID != null){
+          Admin.findByPk(session.adminID)
+          .then(data => {
+            if(data.dataValues.role = "Admin")
+            {
+              next();
+              return;
+            }
+
+          })
+      }
+       
       
     }
     else{
       //Error
     }
 
-  }
 }
 
 
-
- 
-
-
-//checks to see if they are an advisor or an admin, how do I include each?
-
-
-
-//how do I check for any role?
+//having curlybrace errors
 
 
 const authJwt = {
   authenticate: authenticate,
   isAdmin: isAdmin,
-  isAdvisor: this.isAdvisor,
-  isAdminOrAdvisor: isAdminOrAdvisor,
+  isAdvisorOrAdmin: isAdvisorOrAdmin,
   isAny: isAny
   
 };
